53741778c574c34d942be32860710533e76de26f
nanodesu88 <archon.saratov@gmail.com>
2019-06-28 14:04:28 +0200

    Register методы в основных диспетчерах

--- Projects/MyGame/MyGameDispatchers.fs
+++ Projects/MyGame/MyGameDispatchers.fs
@@ -5,8 +5,17 @@ open Nu
 type MainScreenDispatcher() =
   inherit ScreenDispatcher()
   
+  override this.Register (screen, world) =
+    world
+  
 type MainLayerDispatcher() =
   inherit LayerDispatcher()
   
+  override this.Register (layer, world) =
+    world
+  
 type PlayerEntityDispatcher() =
   inherit EntityDispatcher()
+  
+  override this.Register (entity, world) =
+    world
